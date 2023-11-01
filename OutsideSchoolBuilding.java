c18fc96cfe0d4c8ebb7ad99f22c5a8726986dc32
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-01 11:38:11 -0400

    Update OutsideSchoolBuilding.java

--- OutsideSchoolBuilding.java
+++ OutsideSchoolBuilding.java
@@ -5,7 +5,8 @@ public class OutsideSchoolBuilding extends State{
 	public OutsideSchoolBuilding(Model model) {
 		this.model = model;
 
-		State getDescription =
-		
+		State getDescription = "You decided to go to school. You walk very quickly and make it outside the school building just in time. There, you see your best friend and decide to talk to her. You two have been best friends since childhood. After a leghthy conversation, she asks you if you want to skip class and hang out in the Playground. Do you want to skip class? Type 1 for yes. Type 2 for no."  
+		State getOptions;
+		State processChoice(int choice) ;
 	}
 }
