2d59ff989f2f78ba726c47bc8ac72a571ece4081
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-01 11:43:53 -0400

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -5,7 +5,8 @@ public class Classroom extends State{
 	public Classroom(Model model) {
 		this.model = model;
 
-		State getDescription =
-		
+		State getDescription = "You decide to go to class since you are taking a test today. Your friend meets you in the classroom after the class period is over. While there she asks you if you want to come to her club meeting. She is the Vice President of the Musical Theater Club. Do you want to join her? Type 1 for yes. Type 2 for no."
+		State getOptions;
+		State processChoice(int choice) ;
 	}
 }
