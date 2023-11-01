412fc75752aa331e0ae53a09a2851f1714b1a8af
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-01 11:29:33 -0400

    Update StreetOutsideHouse.java

--- StreetOutsideHouse.java
+++ StreetOutsideHouse.java
@@ -6,7 +6,7 @@ public class StreetOutsideHouse extends State{
 	public StreetOutsideHouse(Model model) {
 		this.model = model;
 		
-		State getDescription = "You start the game on the street outside your house. School starts in 20 minutes. Do you want to go to school today?";
+		State getDescription = "You start the game on the street outside your house. School starts in 20 minutes. Do you want to go to school today? Type 1 for yes. Type 2 for no.";
 		State getOptions;
 		State processChoice(int choice) ;
 	}
