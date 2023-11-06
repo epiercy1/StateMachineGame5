b78c17a4f87df6c238ed1c37fc6474386349fa1b
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-06 11:31:30 -0500

    Update LivingRoom.java

--- LivingRoom.java
+++ LivingRoom.java
@@ -4,5 +4,9 @@ public class LivingRoom extends State{
 	
 	public LivingRoom(Model model) {
 		this.model = model;
+
+		State getDescription = "";
+		State getOptions;
+		State processChoice(int choice);
 	}
 }
