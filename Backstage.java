bcd9f2fdf39a843ccfb0fc7b1502fa2e4c93dfba
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-06 11:32:06 -0500

    Update Backstage.java

--- Backstage.java
+++ Backstage.java
@@ -4,5 +4,9 @@ public class Backstage extends State{
 	
 	public Backstage(Model model) {
 		this.model = model;
+
+		State getDescription = "";
+		State getOptions;
+		State processChoice(int choice);
 	}
 }
