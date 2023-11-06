9561422433c93c974b938eb14ff44c4c45ef1397
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-11-06 11:02:49 -0500

    Update Playground.java

--- Playground.java
+++ Playground.java
@@ -5,7 +5,8 @@ public class Playground extends State{
 	public Playground(Model model) {
 		this.model = model;
 
-		State getDescription =
-		
+		State getDescription = "You decide to go to the playground with your friend since you are not doing anything important in class today other than lectures. After a while of hanging out, she asks you if you want to go with her to her club meeting. She is the Vice President of the Musical Theater Club. Do you want to go to her club meeting. Type 1 for yes. Type 2 for no."
+		State getOptions;
+		State processChoice(int choice) ;
 	}
 }
